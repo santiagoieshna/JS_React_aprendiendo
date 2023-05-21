7617db74830a0efe8cfab683f737b35e2a63eaa2
xtalo <117736097+santiagoieshna@users.noreply.github.com>
2023-05-21 15:47:19 +0200

    MovieDetils Fix

--- React/Peliculas02/src/pages/MovieDetails.jsx
+++ React/Peliculas02/src/pages/MovieDetails.jsx
@@ -5,16 +5,20 @@ import { get } from '../data/httpClient';
 import { getMovieImg } from '../utils/getMovieImg';
 
 function MovieDetails(){
-    const {movieId} = useParams;
-    console.log(movieId)
-    const [movie, setMovie]= useState(null);
+    // Cogemos la id por los parametros
+    const {movieId} = useParams();
+    // Declaramos un estado -> iniciado Array vacio [] (no tendra nada hasta que no carguemos data)
+    const [movie, setMovie]= useState([]);
+    // Establecemos el estado
     useEffect(()=>{
         get("/movie/"+movieId).then((data)=>{
             setMovie(data);
         });
     },[movieId]);
     // Esta ultima parte no la entiendo
-    // Porque entre [movieId]¿?
+    // Porque entre [movieId]¿? -> Indica que nuestro efecto depende del valor movieId
+
+   
     const imageUrl = getMovieImg(movie.poster_path, 500)
 
     return(
